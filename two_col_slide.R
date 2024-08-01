44c8a047e90a7297a6e2a0faef686d8bd7d38b52
Tomasz Woźniak <wozniak.tom@pm.me>
2024-08-01 17:24:00 +1000

    nearly final!

---
+++ two_col_slide.R
@@ -0,0 +1,15 @@
+## {background-color="#00bf63"}
+
+:::: {.columns}
+
+::: {.column width="50%"}
+![](bsvars.png){width="150"}
+
+:::
+  
+::: {.column width="50%"}
+![](bsvarSIGNs.png){width="150"}
+
+:::
+  
+::::
